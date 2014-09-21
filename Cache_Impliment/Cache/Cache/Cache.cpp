1b77c1f3547d1120c0ae488c304280624cc885e3
Frank <tlan16@sina.com>
2014-09-22 04:00:34 +1000

    cache handling on matrix A, row work, not tested

--- Cache_Impliment/Cache/Cache/Cache.cpp
+++ Cache_Impliment/Cache/Cache/Cache.cpp
@@ -6,11 +6,17 @@
 // diverging from the structure presented below.
 
 #include "stdafx.h"
+#include <stdio.h>
 #include <iostream>
+#include <bitset>
+#include <iomanip>
+#define _WIN32_WINNT 0x0602
+#include <windows.h>
 using namespace std;
 
 int cache_simulator(int Ways, int Data_Size_kB, int Words_Per_Bock, int Hit_Time)
 {
+
 	// Print input auguments
 	cerr << "Data_Size: " << Data_Size_kB << "kB" << endl;
 	cerr << "Ways: " << Ways << endl;
@@ -46,15 +52,164 @@ int cache_simulator(int Ways, int Data_Size_kB, int Words_Per_Bock, int Hit_Time
 	cerr << "Sets: " << Sets << endl;
 	cerr << "Index_Size: " << Index_Size << "bit" << endl;
 	cerr << "Tag_Bits: " << Tag_Bits << "bit" << endl;
+	cerr << "Address_Bits_Per_Block: " << Address_Bits_Per_Block << "bit" << endl;
 	cerr << "=====================================" << endl;
+
+	int** Valid = new int*[Ways];	// Array to store Valid Bits
+	for (int i = 0; i < Ways; i++)
+		Valid[i] = new int[Lines];
+
+	int** Tag = new int*[Ways];		// Array to store Tag bits.
+	for (int i = 0; i < Ways; i++)
+		Tag[i] = new int[Lines];
+
+	int** LRU = new int*[Ways];	// Array to store LRU Bits
+	for (int i = 0; i < Ways; i++)
+		LRU[i] = new int[Lines];
+
+	// Declare any required variables in this section
+	int time = 0;
+
+	int Matrix_Size_fixed = 3;
+	for (int Matrix_Size = Matrix_Size_fixed; Matrix_Size <= Matrix_Size_fixed; Matrix_Size++) // Step through all matrix sizes
+	{
+		// Initialize Cache as being empty
+		for (int i = 0; i < Ways; i++)
+		{
+			for (int j = 0; j < Lines; j++)
+			{
+				Valid[i][j] = 0;
+			}
+		}
+
+		// Matrix array pointers
+		int Start_Pointer_A = 100000;
+		int Start_Pointer_B = 200000;
+		int Start_Pointer_C = 300000;
+		const int DRAM_Row_Size = 10240;
+
+		time += 10; // Add delay that exists before entering main loop
+		for (int i = 0; i < Matrix_Size; i++)
+		{
+			time += 10; // Add outer loop delay 
+			for (int j = 0; j < Matrix_Size; j++)
+			{
+				time += 10; // Add middle loop delay 
+
+				for (int k = 0; k < Matrix_Size; k = k++)
+				{
+					// Add inner loop delay.
+					time += 10;
+
+					// Calculate Read Address for Matrix A[i][k]
+					int Read_Address_A = (Start_Pointer_A + k + i*Matrix_Size) << 2;// Calculate next lw address for matrix A
+					int Read_Address_B = (Start_Pointer_B + k*Matrix_Size + j) << 2;// Calculate next lw address for matrix B
+					int Read_Address_C = (Start_Pointer_C + j + i*Matrix_Size) << 2;// Calculate next lw address for matrix C
+
+					cerr <<  "A: " << Read_Address_A << ", Tag: " << Read_Address_A / ((Block_size / 8) * Sets) << ", Index: " << (Read_Address_A / (Block_size / 8)) % Sets
+						<< ". B: " << Read_Address_B << ", Tag: " << Read_Address_B / ((Block_size / 8) * Sets) << ", Index: " << (Read_Address_B / (Block_size / 8)) % Sets
+						<< ". C: " << Read_Address_C << ", Tag: " << Read_Address_B / ((Block_size / 8) * Sets) << ", Index: " << (Read_Address_B / (Block_size / 8)) % Sets
+						<< endl;
+
+					// Calculate Tag
+					int Read_Tag_A = Read_Address_A / ((Block_size / 8) * Sets);
+					int Read_Tag_B = Read_Address_B / ((Block_size / 8) * Sets);
+					int Write_Tag_C = Read_Address_C / ((Block_size / 8) * Sets);
+
+					// Calculate Index
+					int Index_A = (Read_Address_A / (Block_size / 8)) % Sets;
+					int Index_B = (Read_Address_B / (Block_size / 8)) % Sets;
+					int Index_C = (Read_Address_C / (Block_size / 8)) % Sets;
+
+					// Deference Index for each way and check each tag and valid bit.
+					boolean hit_A = false;
+					boolean hit_B = false;
+					boolean hit_C = false;
+					int hit_A_counter=0;
+					int hit_B_counter=0;
+					int hit_C_counter=0;
+					boolean Previous_RAM_Row_Valid_A = false;
+					int Current_RAM_Row_A;
+					int Previous_RAM_Row_A;
+					for (int l = 0; l<Ways; l = l + 1) // Check each way
+					{
+						if (Valid[l][Index_A] & (Tag[l][Index_A] == Read_Tag_A))
+						{
+							hit_A = true;
+						}
+						if (Valid[l][Index_B] & (Tag[l][Index_B] == Read_Tag_A))
+						{
+							hit_B = true;
+						}
+						if (Valid[l][Index_C] & (Tag[l][Index_C] == Write_Tag_C))
+						{
+							hit_C = true;
+						}
+					}
+
+					if (hit_A)
+					{
+						time += 10;// If hit, add hit time
+						hit_A_counter++;
+					}
+					else
+					{
+						// If miss calculate miss time.
+						// Calculate RAM row.
+						Current_RAM_Row_A = Read_Address_A / DRAM_Row_Size;
+						if (Previous_RAM_Row_Valid_A & (Current_RAM_Row_A == Previous_RAM_Row_A) )
+						{
+							time += 10; // Caculate number of CAS delays needed.
+						}
+						else // Need RAS and CAS
+						{
+							Previous_RAM_Row_A = Current_RAM_Row_A; // Update RAM row
+							time += 10; // Calculate RAS and CAS delays needed.
+						}
+						Previous_RAM_Row_Valid_A = true; // On power up, a RAS is always needed.
+
+						// Update Cache with new data
+						int Update_Way = LRU[Ways - 1][Index_A];// Choose your way strategy (random,LRU, ~LRU)
+
+						Valid[Update_Way][Index_A] = true; // Write to the way you chose in previous line
+						Tag[Update_Way][Index_A] = Read_Tag_A; // Write to the way you chose in previous line
+
+						for (int i = 1; i < Ways; i++)	// Update LRU order
+						{
+							LRU[i][Index_A] = LRU[i - 1][Index_A];
+							LRU[0][Index_A] = Update_Way;
+						}
+
+					}
+					if (hit_B)
+					{
+						time += 10;// If hit, add hit time
+						hit_B_counter++;
+					}
+					if (hit_C)
+					{
+						time += 10;// If hit, add hit time
+						hit_C_counter++;
+					}
+
+				}
+				cerr << endl;
+			}
+			cerr << endl;
+			cerr << endl;
+		}
+	}
 
 	return 0;
 }
 
+
 int _tmain(int argc, _TCHAR* argv[]) // Some of the below constants you might want to pass as program arguments
 {
+
 	// Ways, Data_Size_kB, Words_Per_Bock, Hit_Time
-	cache_simulator(1, 8, 2, 1);
+	cache_simulator(4, 4, 1, 1);
+	/*
 	cache_simulator(1, 32, 2, 2);
 	cache_simulator(1, 512, 2, 3);
 	
@@ -70,8 +225,12 @@ int _tmain(int argc, _TCHAR* argv[]) // Some of the below constants you might wa
 	cache_simulator(4, 32, 8, 2);
 	cache_simulator(4, 512, 8, 3);
 
+	cache_simulator(1, 8, 4, 1);
+	*/
+
 
 	std::cin.get();
 	return 0;
 }
+
 
